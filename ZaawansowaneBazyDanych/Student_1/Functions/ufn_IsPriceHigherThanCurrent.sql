0d38a8d2ef60f936ead24897614030ac71b30204
Kacper <przelozny14@gmail.com>
2025-12-21 22:16:29 +0100

    lab-4

---
+++ ZaawansowaneBazyDanych/Student_1/Functions/ufn_IsPriceHigherThanCurrent.sql
@@ -0,0 +1,20 @@
+﻿CREATE FUNCTION [Student_1].ufn_IsPriceHigherThanCurrent
+(
+	@Product NVARCHAR(MAX)
+)
+RETURNS BIT AS
+BEGIN
+	DECLARE @ProductID INT = JSON_VALUE(@Product, '$.ProductID')
+	DECLARE @Price MONEY = JSON_VALUE(@Product, '$.ListPrice')
+	DECLARE @Result BIT;
+
+	DECLARE @ActualPrice MONEY;
+	SELECT @ActualPrice = ListPrice FROM SalesLT.Product WHERE Product.ProductID = @ProductID
+
+	IF @Price > @ActualPrice
+		SET @Result = 1;
+	IF @Price < @ActualPrice
+		SET @Result = 0;
+
+	RETURN @Result;
+END;
---
+++ ZaawansowaneBazyDanych/Student_1/Functions/ufn_ProductsJsonByCategory.sql
@@ -0,0 +1,23 @@
+﻿CREATE FUNCTION [Student_1].ufn_ProductsJsonByCategory
+(
+	@CategoryName NVARCHAR(50)
+)
+RETURNS NVARCHAR(MAX) AS
+BEGIN
+	DECLARE @json NVARCHAR(MAX)
+
+	SET @json = (
+		SELECT 
+			Product.ProductID as 'ProductID',
+			Product.Name as 'Name',
+			Product.Color as 'Color',
+			Product.Size as 'Size',
+			Product.Weight as 'Weight'
+		FROM SalesLT.Product
+		INNER JOIN SalesLT.ProductCategory ON Product.ProductCategoryID = ProductCategory.ProductCategoryID
+		WHERE ProductCategory.Name = @CategoryName
+		FOR JSON PATH
+    );
+    
+    RETURN @json;
+END;
