12d2b6e94ddc573b55a8acdec4258d11df4ebe36
Kacper <przelozny14@gmail.com>
2025-12-21 21:16:27 +0100

    lab-3

---
+++ ZaawansowaneBazyDanych/SalesLT/Tables/ProductAttribute.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [SalesLT].[ProductAttribute] (
+    [ProductID]  INT                                    NULL,
+    [Attributes] XML(CONTENT [dbo].[ProductAttributes]) NULL,
+    CONSTRAINT [FK_ProductAttribute_Product] FOREIGN KEY ([ProductID]) REFERENCES [SalesLT].[Product] ([ProductID])
+);
+
---
+++ ZaawansowaneBazyDanych/dbo/XMLSchemaCollections/ProductAttributes.sql
@@ -0,0 +1,18 @@
+﻿CREATE XML SCHEMA COLLECTION [dbo].[ProductAttributes]
+    AS N'<xsd:schema xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <xsd:element name="Attributes" type="Attribute" />
+  <xsd:complexType name="Attribute">
+    <xsd:complexContent>
+      <xsd:restriction base="xsd:anyType">
+        <xsd:sequence>
+          <xsd:element name="Name" type="xsd:string" />
+          <xsd:element name="ProductNumber" type="xsd:string" />
+          <xsd:element name="Color" type="xsd:string" />
+          <xsd:element name="ListPrice" type="xsd:decimal" />
+          <xsd:element name="Category" type="xsd:string" />
+        </xsd:sequence>
+      </xsd:restriction>
+    </xsd:complexContent>
+  </xsd:complexType>
+</xsd:schema>';
+
